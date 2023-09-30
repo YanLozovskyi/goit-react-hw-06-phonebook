e14e12c69e9f5b8f45a34669890bb7ea832ce3d9
YanLozovskyi <yanlozovskiy@gmail.com>
2023-09-30 10:52:48 +0300

    deleted helpers

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,10 @@
 import { FilterLabel, FilterInput } from './Filter.styled';
 import { setFilter } from 'redux/filterSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { selectFilter } from 'redux/selectors';
 
 export const Filter = () => {
-  const filter = useSelector(state => state.filter);
+  const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
   return (
     <>
